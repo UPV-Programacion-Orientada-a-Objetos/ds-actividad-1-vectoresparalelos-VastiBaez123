ea7811dd0115568d0e27432239a0e0f0287657e4
VastiBaez123 <2430010@upv.edu.mx>
2025-09-23 11:32:29 -0600

    ultimo cambio

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <map>
 
 // Prototipos de las funciones 
 int contarL(const std::string& RutArch);
@@ -12,12 +11,12 @@ void leerArch(const std::string& RutArch,
               std::string disciplinas[],
               std::string generos[],
               int medallas[]);
-void buscarAtleta(const std::string nombres[],
-                  const std::string paises[],
-                  const std::string disciplinas[],
-                  const std::string generos[],
-                  const int medallas[],
-                  int numRegistros);
+void buscarAt(std::string nombreAt[], 
+              std::string paises[], 
+              std::string disciplina[],
+              std::string genero[],
+              const int medallas[], 
+              const int registros);
 void calcularMedallasPorPais(const std::string paises[], const int medallas[], int numRegistros);
 void encontrarAtletaMasMedallas(const std::string nombres[], const int medallas[], int numRegistros);
 
@@ -27,18 +26,18 @@ int main(int argc, char* argv[]) {
     if (argc > 1) {
         RutArch = argv[1];
     } else {
-        std::cout << "Por favor, introduce la ruta al archivo de datos de atletas: ";
+        std::cout << "Por favor, introduce la ruta al archivo del registro: ";
         std::cin >> RutArch;
     }
 
     int numRegistros = contarL(RutArch);
 
     if (numRegistros <= 0) {
-        std::cerr << "El archivo esta vacio o no se pudo abrir." << std::endl;
+        std::cerr << "El archivo esta vacio o no se pudo abrir" << std::endl;
         return 1;
     } 
 
-    std::cout << "Archivo abierto, tiene " << numRegistros - 1 << " registros de atletas." << std::endl;
+    std::cout << "Archivo abierto " << numRegistros - 1 << " registros de atletas." << std::endl;
     
     std::string nombres[numRegistros];
     std::string paises[numRegistros];
@@ -62,7 +61,7 @@ int main(int argc, char* argv[]) {
 
         switch (opcion) {
             case 1:
-                buscarAtleta(nombres, paises, disciplinas, generos, medallas, numRegistros);
+                buscarAt(nombres, paises, disciplinas, generos, medallas, numRegistros - 1);
                 break;
             case 2:
                 calcularMedallasPorPais(paises, medallas, numRegistros);
@@ -137,73 +136,99 @@ void leerArch(const std::string& RutArch,
                 }
                 i++;
             } else {
-                std::cout << "Advertencia: Se ignoro una linea con formato incorrecto. \n";
+                std::cout << "formato incorrecto compañere \n";
             }
         }
         archivo.close();
     }
 }
 
-void buscarAtleta(const std::string nombres[],
-                  const std::string paises[],
-                  const std::string disciplinas[],
-                  const std::string generos[],
-                  const int medallas[],
-                  int numRegistros) {
-    std::string nombreBusc;
-    std::cout << "Cual nombre quieres buscar: ";
-    std::cin.ignore();
-    std::getline(std::cin, nombreBusc);
-
-    bool encontrado = false;
-    for (int i = 0; i < numRegistros; i++){
-        if (nombres[i] == nombreBusc){
-            std::cout << "\ninfo de atleta" << std::endl;
-            std::cout << "Nombre: " << nombres[i] << std::endl;
+void buscarAt(std::string nombreAt[], 
+              std::string paises[], 
+              std::string disciplina[],
+              std::string genero[],
+              const int medallas[], 
+              const int registros) {
+    std::string busqueda;
+    std::cout << "Ingrese el nombre del atleta: ";
+    std::getline(std::cin, busqueda);
+    
+    for (int i = 0; i < registros; i++) {
+        if(nombreAt[i] == busqueda){
+            std::cout << "Nombre: " << nombreAt[i] << std::endl;
             std::cout << "Pais: " << paises[i] << std::endl;
-            std::cout << "Disciplina: " << disciplinas[i] << std::endl;
-            std::cout << "Genero: " << generos[i] << std::endl;
-            std::cout << "Medallas de oro: " << medallas[i] << std::endl;
-            encontrado = true;
-            break;
+            std::cout << "Disciplina: " << disciplina[i] << std::endl;
+            std::cout << "Genero: " << genero[i] << std::endl;
+            std::cout << "Medallas: " << medallas[i] << std::endl;
+            return; // Sale de la función cuando encuentra el atleta
         }
     }
-    if (!encontrado){
-        std::cout << "Atleta no encontrado." << std::endl;
-    }
+    // Si llega aquí no encontró el atleta
+    std::cout << "no estaba" << std::endl;
 }
 
 void calcularMedallasPorPais(const std::string paises[], const int medallas[], int numRegistros) {
-    std::map<std::string, int> totalesPorPais;
-
-    for (int i = 0; i < numRegistros; ++i) {
-        totalesPorPais[paises[i]] += medallas[i];
-    }
-
-    std::cout << "\nTotal de medallas por Pais:\n";
-    for (const auto& par : totalesPorPais) {
-        std::cout << "Pais: " << par.first << " - Medallas: " << par.second << std::endl;
-    }
-}
-
-void encontrarAtletaMasMedallas(const std::string nombres[], const int medallas[], int numRegistros) {
-    int maxMedallas = -1;
-    int indiceGanador = -1;
-
-    for (int i = 0; i < numRegistros; ++i) {
-        if (medallas[i] > maxMedallas) {
-            maxMedallas = medallas[i];
-            indiceGanador = i;
+    const int MAX_PAISES_UNICOS = 100; 
+    std::string paisesUnicos[MAX_PAISES_UNICOS];
+    int medTotales[MAX_PAISES_UNICOS] = {0};
+    int cont = 0;
+    
+    for (int i = 0; i < numRegistros; i++) {
+        bool encontrado = false;
+        
+        for (int j = 0; j < cont; j++) {
+            if (paises[i] == paisesUnicos[j]) {
+                medTotales[j] += medallas[i];
+                encontrado = true;
+                break;
+            }
+        }
+        
+        if (!encontrado && cont < MAX_PAISES_UNICOS) {
+            paisesUnicos[cont] = paises[i];
+            medTotales[cont] = medallas[i];
+            cont++;
         }
     }
-
-    if (indiceGanador != -1) {
-        std::cout << "\nAtleta con mas medallas de oro:\n";
-        std::cout << "Nombre: " << nombres[indiceGanador] << std::endl;
-        std::cout << "Medallas de oro: " << maxMedallas << std::endl;
-    } else {
-        std::cout << "No se encontro ningun atleta.\n";
+    
+    std::cout << "\nMedallas por país:\n";
+    for (int i = 0; i < cont; i++) {
+        std::cout << "País: " << paisesUnicos[i] << " - Medallas: " << medTotales[i] << std::endl;
     }
+    return;
 }
-//disculpeme que le deje esto esque asi corre nadamas y no me aprendo aun el como corre
-//vasti@vasti-Aspire-A514-54:~/Música/act1/act1$ output/main data/data1.csv
+
+
+void calcularMedallasPorPais(const std::string paises[], const int medallas[], int numRegistros) {
+    const int tamañomax = 100; 
+    std::string paisesQueYaConte[tamañomax];
+    int medallasPorPais[tamañomax] = {0};
+    int contadorDePaisesUnicos = 0;
+    
+    for (int i = 0; i < numRegistros; i++) {
+        bool paisEncontrado = false;
+        
+        for (int j = 0; j < contadorDePaisesUnicos; j++) {
+            if (paises[i] == paisesQueYaConte[j]) {
+                medallasPorPais[j] += medallas[i];
+                paisEncontrado = true;
+                break;
+            }
+        }
+        
+        if (!paisEncontrado && contadorDePaisesUnicos < tamañomax) {
+            paisesQueYaConte[contadorDePaisesUnicos] = paises[i];
+            medallasPorPais[contadorDePaisesUnicos] = medallas[i];
+            contadorDePaisesUnicos++;
+        }
+    }
+    
+    std::cout << "\nMedallas por país:\n";
+    for (int i = 0; i < contadorDePaisesUnicos; i++) {
+        std::cout << "País: " << paisesQueYaConte[i] << " - Medallas: " << medallasPorPais[i] << std::endl;
+    }
+    return;
+}
+//comando para correrlo
+//cd /home/vasti/Música/act1/act1
+//output/main data/data1.csv
